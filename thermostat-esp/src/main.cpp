4e05f58b3ceb81896b84a21e6b0a9b39580ad21b
Oleg Artemenko <artem@kb51.ru>
2021-05-22 22:33:17 +0700

    Сделал управление реле

--- thermostat-esp/src/main.cpp
+++ thermostat-esp/src/main.cpp
@@ -13,11 +13,23 @@ typedef struct {
 volatile CurTime curTime{0,0};
 bool dhtSynced = false; 
 time_t dhtLastUpdate = 0; 
+bool _releState[RELE_COUNT];
+
+
+void setRele(uint8_t num,bool state) {
+  int pins[] = RELE_PINS;
+  DBG("Set rele #%d %s",num, state ? "ON":"OFF");
+  _releState[num] = state;
+  digitalWrite(pins[num],state);
+}
 
 bool execRequest(String topic, String data) {
   const char* pData = data.c_str();
   const char* pTopic = topic.c_str();
   DBG("Got request %s = %s",pTopic,pData);
+  if(topic == "setRele01") {
+    setRele(0,(data == "1"));
+  }
   return true;
 }
 
@@ -77,6 +89,13 @@ DBG("Start..");
   os_timer_arm(&myTimer,1,true);
   netInit(WIFI_SSID, WIFI_PASS, MQTT_SERVER, MQTT_CLIENT_ID, MQTT_BASE);
 //  attachInterrupt(D5,czInterrupt,RISING);
+
+  int pins[] = RELE_PINS;
+  for(uint8_t i=0; i< RELE_COUNT;i++) {
+    pinMode(pins[i],OUTPUT);
+    setRele(i,false);    
+  }
+  
 }
 
 void loop() {
